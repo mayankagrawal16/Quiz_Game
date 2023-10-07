b40196515d902cf1763b8f03baac4a9e35f8d4f3
mayank <mayank.agrawal1604@gmail.com>
2023-10-07 11:06:11 +0530

    second Commit

---
+++ app/src/main/java/com/mayank/quiz_game/Splash_Screen.java
@@ -0,0 +1,13 @@
+package com.mayank.quiz_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
